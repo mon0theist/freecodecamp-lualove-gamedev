7c0e2d6f71488d8d112ce20af37cbca03d389631
mon0theist <bburns91@gmail.com>
2026-03-26 15:24:38 -0500

    Drawing and moving shapes

--- main.lua
+++ main.lua
@@ -1,19 +1,54 @@
-_G.love = require("love") -- load "love" lua module
+_G.love = require("love") -- load "love" lua module, _G = global
 
 -- Basic functions
 -- Every game needs these 3 functions (I think?)
+--[[ 
+from https://love2d.org/wiki/love:
+
+love.load to do one-time setup of your game
+love.update which is used to manage your game's state frame-to-frame
+love.draw which is used to render the game state onto the screen.
+--]]
+
 function love.load() 
     -- load game data upon booting
-    _G.number = 0
+    _G.Number = 0
+    love.graphics.setBackgroundColor(0.5, 0.5, 1)
+    
+    _G.pacman = {
+        x = 200,
+        y = 250,
+        eat = false,
+    }
+
+    _G.food_x = 600
 end
 
 function love.update(dt) 
     -- runs at 60fps, game logic?
     -- dt = delta time, basically dt = frame
-    number = number + 1
+    Number = Number + 1
+    pacman.x = pacman.x + 1
+
+    if pacman.x >= food_x + 20 then
+        pacman.eat = true
+    end
 end
 
 function love.draw()
     -- draw visuals to screen
-    love.graphics.print(number)
+    
+    -- frames in upper-left corner
+    love.graphics.print(Number)
+
+    if not pacman.eat then
+        love.graphics.setColor(0, 0, 0)
+        love.graphics.rectangle("fill", food_x, 200, 70, 70)    
+    end
+
+    love.graphics.circle("line", 100, 100, 50)    
+
+    -- pacman
+    love.graphics.setColor(1, 0.7, 0.1)
+    love.graphics.arc("fill", pacman.x, pacman.y, 60, 1, 5)
 end
