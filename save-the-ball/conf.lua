eb9b43a0c16813c8189440d6c952d92e72245110
Ryen Burns <bburns91@gmail.com>
2026-03-30 15:13:19 -0500

    Game Setup

---
+++ save-the-ball/conf.lua
@@ -0,0 +1,8 @@
+---@diagnostic disable: duplicate-set-field
+-- https://love2d.org/wiki/Config_Files
+-- t = table
+function love.conf(t)
+    -- t.window.display = 2 
+    -- make game appear on second monitor (not applicable on laptop)
+    -- t.window.title = "Save The Ball" -- also can be done in main.lua under love.load()
+end
