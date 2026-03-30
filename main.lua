40ab706fb78e022eb03dad547c8d8445d9d468b9
Abdul-Hakeem <bburns91@gmail.com>
2026-03-29 20:12:21 -0500

    Working with Sprites

--- main.lua
+++ main.lua
@@ -28,7 +28,7 @@ function love.load()
         },
         animation = {
             direction = "right",
-            idle = false,
+            idle = true,
             frame = 1,
             max_frames = 8,
             speed = 20,
@@ -51,20 +51,44 @@ end
 function love.update(dt) 
     -- runs at 60fps, game logic?
     -- dt = delta time, basically dt = frame
-    if not jack.animation.idle then 
-        
-        jack.animation.timer =  jack.animation.timer + dt
 
-        if jack.animation.timer > 0.2 then
+    if love.keyboard.isDown("d") then
+        jack.animation.idle = false
+        jack.animation.direction = "right"
+    elseif love.keyboard.isDown("a") then
+        jack.animation.idle = false
+        jack.animation.direction = "left"
+    else
+        jack.animation.idle = true
+        jack.animation.frame = 1
+    end
+
+    if not jack.animation.idle then
+        jack.animation.timer = jack.animation.timer + dt -- keeps game animation consistent to real world time vs processing time?
+
+        if jack.animation.timer > 0.2 then -- Change the animation frame every 0.2 seconds
+            -- once 0.2 seconds has been reached then...
+            -- reset the timer back to 0.1 (why not 0? not sure)
             jack.animation.timer = 0.1
-            
+
+            -- advance the animation to the next frame
             jack.animation.frame = jack.animation.frame + 1
+        
+            if jack.animation.direction == "right" then
+                jack.x = jack.x + jack.animation.speed
+            elseif jack.animation.direction == "left" then
+                jack.x = jack.x - jack.animation.speed
+            end
+
+            -- reset back to frame 1 after reaching the last frame
             if jack.animation.frame > jack.animation.max_frames then
                 jack.animation.frame = 1
             end
+
         end
     end
 end
+
 
 function love.draw()
     -- draw visuals to screen
